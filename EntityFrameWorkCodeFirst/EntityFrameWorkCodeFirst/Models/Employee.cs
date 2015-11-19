939c4da3c2e9838e0004b4260a159c6699c811cf
RizwanRumi <rizwan.cse33@gmail.com>
2015-11-19 23:19:10 +0600

    sample upload and Read carefully instruction file

---
+++ EntityFrameWorkCodeFirst/EntityFrameWorkCodeFirst/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameWorkCodeFirst.Models
+{
+    [Table("tbl_employee")]
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
